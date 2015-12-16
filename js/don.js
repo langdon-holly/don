d96603b5eee1cec0e964cb18e455cd7ca2b7af98
langdon-holly <langdonzh@gmail.com>
2015-12-16 08:08:33 -0500

    starting parser

---
+++ js/don.js
@@ -0,0 +1,11 @@
+function expr(str) {
+  if (str.charAt(0) === '(') {
+    return form(str);
+  }
+}
+
+function form(str) {
+  if (str.charAt(0) === '('
+      && str.charAt(str.length-1) === ')') {
+  }
+}
