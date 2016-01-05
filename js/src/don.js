2b9e544f85e2c752f01ef0e0ce398a14b5c86f49
langdon-holly <langdonzh@gmail.com>
2016-01-05 08:16:22 -0500

    stuff

--- js/src/don.js
+++ js/src/don.js
@@ -21,11 +21,25 @@ function seq() {
     if (args.length == 1) return args[0];
     else if (args.length == 2) {
       for (var betweenI = str.length; betweenI >= 0; betweenI--) {
-        
+        var firstOne = args[0](str.slice(0, betweenI));
+        if (firstOne[0]) {
+          var lastOne = args[1](str.slice(betweenI, str.length));
+          if (lastOne[0]) {
+            return [true, [firstOne[1], lastOne[1]]];
+          }
+        }
       }
+      return [false];
     }
     else if (args.length > 2) {
       return seq(args[0], seq.apply(this, Array.prototype.slice.call(args, 1, args.length)));
     }
   }
 }
+
+function string(str0) {
+  return function(str1) {
+    return str0 === str1;
+  }
+}
+
