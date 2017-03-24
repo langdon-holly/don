21589cf00a97c579cd204fd743e31dd6102c356c
Langdon Holly <langdonzh@gmail.com>
2017-03-24 08:58:56 -0400

    src/don.js reads from STDIN unless argument is passed

--- src/don.js
+++ src/don.js
@@ -16,34 +16,44 @@ function indexToLineColumn(index, string) {
       line++;
       col = 1;}
     else col++;}
-  throw new RangeError("indexToLineColumn: index=" + index + " is out of bounds");}
+  throw new RangeError
+            ("indexToLineColumn: index=" + index + " is out of bounds");}
 
-fs.readFile(program.args[0], 'utf8', function(err, data) {
-  if (err) {
-    console.log("Couldn't read file");
-    process.exit(1);}
+fs.readFile
+( program.args.length > 0 ? program.args[0] : 0
+, 'utf8'
+, function(err, data)
+    { if (err) {
+        console.log("Couldn't read file");
+        process.exit(1);}
 
-  var parsed = don.parse(data);
+      var parsed = don.parse(data);
 
-  if (parsed[0]) {
-    don.topEval.apply(this, parsed.slice(1));
-    process.exit(0);}
-  else {
-    var errAt = parsed[1];
-    if (errAt == -1) console.log("Syntax error: " + program.args[0] + " should be at least " + (Array.from(data).length + 1) + " characters");
-    else if (errAt == 0) console.log("Error in the syntax");
-    else {
-      var lineCol = indexToLineColumn(parsed[1] - 1, data);
-      console.log("Syntax error at "
-                  + program.args[0]
-                  + " "
-                  + lineCol[0]
-                  + ","
-                  + lineCol[1]
-                  + ":");
-      console.log(data.split('\n')[lineCol[0] - 1]
-                  + "\n"
-                  + " ".repeat(lineCol[1] - 1)
-                  + "^");}
-    process.exit(2);}});
+      if (parsed[0]) {
+        don.topEval.apply(this, parsed.slice(1));
+        process.exit(0);}
+      else {
+        var errAt = parsed[1];
+        if (errAt == -1)
+          console.log
+          ( "Syntax error: "
+           + program.args[0]
+           + " should be at least "
+           + (Array.from(data).length + 1)
+           + " characters");
+        else if (errAt == 0) console.log("Error in the syntax");
+        else {
+          var lineCol = indexToLineColumn(parsed[1] - 1, data);
+          console.log("Syntax error at "
+                      + program.args[0]
+                      + " "
+                      + lineCol[0]
+                      + ","
+                      + lineCol[1]
+                      + ":");
+          console.log(data.split('\n')[lineCol[0] - 1]
+                      + "\n"
+                      + " ".repeat(lineCol[1] - 1)
+                      + "^");}
+        process.exit(2);}});
 
