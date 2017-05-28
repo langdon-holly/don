f1fad927d8e1ded900fcf2c4d456eaa9d969d3d3
Langdon Holly <langdonzh@gmail.com>
2017-05-27 21:02:54 -0400

    indexToLineColumn returns js object

--- src/don.js
+++ src/don.js
@@ -15,16 +15,19 @@
 ; program.parse(process.argv)
 
 ; function indexToLineColumn(index, string)
-  { var arr = Array.from(string), line = 1, col = 1
+  { var arr = Array.from(string), line = 0, col = 0
   ; for (var i = 0; i < arr.length; i++)
-    { if (i == index) return [line, col]
-    ; if (arr[i] === '\n') line++, col = 1
+    { if (i == index)
+        return {line0: line, col0: col, line1: ++line, col1: ++col}
+    ; if (arr[i] === '\n') line++, col = 0
     ; else col++}
     throw new RangeError
               ("indexToLineColumn: index=" + index + " is out of bounds")}
 
+; var arg0 = program.args[0]
+
 ; fs.readFile
-  ( program.args.length > 0 ? program.args[0] : 0
+  ( program.args.length > 0 ? arg0 : 0
   , 'utf8'
   , function(err, data)
     { if (err)
@@ -41,7 +44,7 @@
         ; if (errAt == -1)
             console.log
             ( "Syntax error: "
-              + program.args[0]
+              + arg0
               + " should be at least "
               + (Array.from(data).length + 1)
               + " characters")
@@ -49,15 +52,15 @@
         ; else
           { var lineCol = indexToLineColumn(parsed[1] - 1, data)
           ; console.log("Syntax error at "
-                        + program.args[0]
+                        + arg0
                         + " "
-                        + lineCol[0]
+                        + lineCol.line1
                         + ","
-                        + lineCol[1]
+                        + lineCol.col1
                         + ":")
-          ; console.log(data.split('\n')[lineCol[0] - 1]
+          ; console.log(data.split('\n')[lineCol.line0]
                         + "\n"
-                        + " ".repeat(lineCol[1] - 1)
+                        + " ".repeat(lineCol.col0)
                         + "^")}
 
           ; var trace = parsed[2]
