0ad20e9f21ba31f31f6d6edf9674855d0be48ab5
Langdon Holly <langdonzh@gmail.com>
2017-08-29 16:31:47 -0400

    list-parsing@0.3.0

--- src/don-parse.js
+++ src/don-parse.js
@@ -13,61 +13,63 @@
 // Stuff
 
 ; function comment()
-  {return (
-     ps.name
-     ( { parseElem
-         : function(elem)
-           {return (
-              ps.or
-              ( ps.seq(ps.string(';'), ows(), expr)
-              , ps.seq
-                ( ps.string('#')
-                , ps.seq
-                  ( ps.many(ps.elemNot(ps.string('\u000A')))
-                  , ps.string('\u000A')))).parseElem(elem))}
-       , match: false
-       , result: undefined
-       , noMore: false
-       , futureSuccess: false
-       , stack: ps.nil}
-     , "comment"))}
+  { return (
+      ps.name
+      ( ps.nilStacked
+        ( { parseElem
+            : function(elem)
+              { return (
+                  ps.parseElem
+                  ( ps.or
+                    ( [ ps.seq([ps.string(';'), ows(), expr()])
+                      , ps.seq
+                        ( [ ps.string('#')
+                          , ps.seq
+                            ( [ ps.many(ps.elemNot([ps.string('\u000A')]))
+                              , ps.string('\u000A')])])])
+                  , elem))}
+          , match: false
+          , result: undefined
+          , noMore: false
+          , futureSuccess: false})
+      , "comment"))}
 
-; function ows() {return ps.many(ps.or(ps.wsChar, comment()))}
+; function ows() {return ps.many(ps.or([ps.wsChar, comment()]))}
 
 ; function call()
-  {return (
-     ps.name
-     ( ps.map
-       ( ps.before
-         ( ps.seq(ps.string('\\'), ows())
-         , ps.between(ps.nothing, expr, ows(), expr))
-       , function(pt) {return ['call', pt]})
-     , "function call"))}
+  { return (
+      ps.name
+      ( ps.map
+        ( ps.before
+          ( ps.seq([ps.string('\\'), ows()])
+          , ps.between([ps.nothing, expr(), ows(), expr()]))
+        , function(pt) {return ['call', pt]})
+      , "function call"))}
 
 ; var nameChar
   = ps.elemNot
-    ( ps.string('(')
-    , ps.string(')')
-    , ps.string('[')
-    , ps.string(']')
-    , ps.string('{')
-    , ps.string('}')
-    , ps.string('`')
-    , ps.wsChar
-    , ps.string('\\')
-    , ps.string('|')
-    , ps.string(';')
-    , ps.string('#')
-    , ps.string('"'))
+    ( [ ps.string('(')
+      , ps.string(')')
+      , ps.string('[')
+      , ps.string(']')
+      , ps.string('{')
+      , ps.string('}')
+      , ps.string('`')
+      , ps.wsChar
+      , ps.string('\\')
+      , ps.string('|')
+      , ps.string(';')
+      , ps.string('#')
+      , ps.string('"')])
 
 ; function name()
-  {return (
-     ps.name
-     ( ps.map
-       ( ps.after(ps.many1(nameChar), ps.wsChar)
-       , function(pt)
-         {return ['ident', pt.map(function(chr) {return chr.codePointAt(0)})]})
-     , "short-identifier"))}
+  { return (
+      ps.name
+      ( ps.map
+        ( ps.after(ps.many1(nameChar), ps.wsChar)
+        , function(pt)
+          {return ['ident', pt.map(function(chr) {return chr.codePointAt(0)})]})
+      , "short-identifier"))}
 
 ; var heredoc
   = ps.name
@@ -77,29 +79,28 @@
           ( ps.string('@')
           , ps.many
             ( ps.or
-              ( ps.elemNot(ps.string('@'), ps.string('\\'))
-              , ps.before
-                ( ps.string('\\')
-                , ps.or(ps.string('@'), ps.string('\\')))))
+              ( [ ps.elemNot([ps.string('@'), ps.string('\\')])
+                , ps.before
+                  ( ps.string('\\')
+                  , ps.or([ps.string('@'), ps.string('\\')]))]))
           , ps.string('@'))
-        , function(endStr)
-          {return (
-             ps.shortest
-             ( ps.after
-               ( ps.anything
-               , ps.seq.apply
-                 ( this
-                 , endStr.map(function(chr) {return ps.string(chr)})))))})
+        , [ function(endStr)
+            { return (
+                ps.shortest
+                ( ps.after
+                  ( ps.anything
+                  , ps.seq
+                    (endStr.map(function(chr) {return ps.string(chr)})))))}])
       , function(pt)
-        {return (
-           [ 'heredoc'
-           , pt.map(function(chr) {return ['char', chr.codePointAt(0)]})])})
+        { return (
+            [ 'heredoc'
+            , pt.map(function(chr) {return ['char', chr.codePointAt(0)]})])})
     , "heredoc")
 
 ; var character
   = ps.name
     ( ps.map
-      ( ps.before(ps.string('`'), ps.elemNot())
+      ( ps.before(ps.string('`'), ps.oneElem)
       , function(pt) {return ['char', pt.codePointAt(0)]})
     , "character-literal")
 
@@ -110,75 +111,77 @@
         ( ps.string('|')
         , ps.many
           ( ps.or
-            ( ps.elemNot(ps.string('\\'), ps.string('|'))
-            , ps.before
-              (ps.string('\\'), ps.or(ps.string('|'), ps.string('\\')))))
+            ( [ ps.elemNot([ps.string('\\'), ps.string('|')])
+              , ps.before
+                (ps.string('\\'), ps.or([ps.string('|'), ps.string('\\')]))]))
         , ps.string('|'))
       , function(pt)
         {return ['ident', pt.map(function(chr) {return chr.codePointAt(0)})]})
     , "long-identifier")
 
 ; function listContents()
-  {return ps.around(ows(), ps.sepBy(expr, ows()), ows())}
+  {return ps.around(ows(), ps.sepBy(expr(), ows()), ows())}
 
 ; function parenCall()
-  {return (
-     ps.name
-     ( ps.map
-       ( ps.around(ps.string("("), listContents(), ps.string(")"))
-       , function(pt) {return ['call', pt]})
-     , "paren-list"))}
+  { return (
+      ps.name
+      ( ps.map
+        ( ps.around(ps.string("("), listContents(), ps.string(")"))
+        , function(pt) {return ['call', pt]})
+      , "paren-list"))}
 
 ; function list()
-  {return (
-     ps.name
-     ( ps.map
-       ( ps.around(ps.string("["), listContents(), ps.string("]"))
-       , function(pt) {return ['bracketed', pt]})
-     , "bracket-list"))}
+  { return (
+      ps.name
+      ( ps.map
+        ( ps.around(ps.string("["), listContents(), ps.string("]"))
+        , function(pt) {return ['bracketed', pt]})
+      , "bracket-list"))}
 
 ; function braced()
-  {return (
-     ps.name
-     ( ps.map
-       ( ps.around(ps.string("{"), listContents(), ps.string("}"))
-       , function(pt) {return ['braced', pt]})
-     , "brace-list"))}
+  { return (
+      ps.name
+      ( ps.map
+        ( ps.around(ps.string("{"), listContents(), ps.string("}"))
+        , function(pt) {return ['braced', pt]})
+      , "brace-list"))}
 
 ; function quote()
-  {return (
-     ps.name
-     ( ps.map
-       ( ps.before
-         ( ps.seq(ps.string('"'), ows())
-         , expr)
-       , function(pt) {return ['quote', pt]})
-     , "quotation"))}
+  { return (
+      ps.name
+      ( ps.map
+        ( ps.before
+          ( ps.seq([ps.string('"'), ows()])
+          , expr())
+        , function(pt) {return ['quote', pt]})
+      , "quotation"))}
 
-; var expr
-  = { parseElem
-      : function(elem)
-        { return (
-            ps.or
-            ( parenCall()
-            , list()
-            , name()
-            , call()
-            , braced()
-            , ident
-            , heredoc
-            , quote()
-            , character)
-            .parseElem(elem))}
-    , match: false
-    , result: undefined
-    , noMore: false
-    , futureSuccess: false
-    , stack: ps.nil}
+; function expr()
+  { return (
+      ps.nilStacked
+      ( { parseElem
+          : function(elem)
+            { return (
+                ps.parseElem
+                ( ps.or
+                  ( [ parenCall()
+                    , list()
+                    , name()
+                    , call()
+                    , braced()
+                    , ident
+                    , heredoc
+                    , quote()
+                    , character])
+                , elem))}
+        , match: false
+        , result: undefined
+        , noMore: false
+        , futureSuccess: false}))}
 
 ; function parseFile(str)
   { var arr = _.toArray(str)
-  ; var parsed = ps.shortestMatch(ps.before(ows(), expr), arr)
+  ; var parsed = ps.shortestMatch(ps.before(ows(), expr()), arr)
   ; return (
       parsed.status === 'match'
       ? _.assign({rest: arr.slice(parsed.index)}, parsed)
