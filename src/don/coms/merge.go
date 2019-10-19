ae6162411d438677cd511bcaca4ad3eff5197c5b
Langdon Holly <langdonzh@gmail.com>
2019-10-18 20:54:39 -0400

    MergeCom can merge any positive number of fields

--- src/don/coms/merge.go
+++ src/don/coms/merge.go
@@ -2,79 +2,100 @@ package coms
 
 import . "don/core"
 
-func runMerge(theType DType, inputA, inputB Input, output Output, quit <-chan struct{}) {
+func listenUnit(input <-chan Unit, output chan<- Unit, quit <-chan struct{}) {
+	for {
+		select {
+		case <-input:
+			output <- Unit{}
+		case <-quit:
+			return
+		}
+	}
+}
+
+func listenRef(input <-chan Ref, output chan<- Ref, quit <-chan struct{}) {
+	for {
+		select {
+		case val := <-input:
+			output <- val
+		case <-quit:
+			return
+		}
+	}
+}
+
+func listenSyntax(input <-chan Syntax, output chan<- Syntax, quit <-chan struct{}) {
+	for {
+		select {
+		case val := <-input:
+			output <- val
+		case <-quit:
+			return
+		}
+	}
+}
+
+func listenCom(input <-chan Com, output chan<- Com, quit <-chan struct{}) {
+	for {
+		select {
+		case val := <-input:
+			output <- val
+		case <-quit:
+			return
+		}
+	}
+}
+
+func runMerge(theType DType, inputs []Input, output Output, quit <-chan struct{}) {
 	switch theType.Tag {
 	case UnitTypeTag:
-		a, b := inputA.Unit, inputB.Unit
-		o := output.Unit
-		for {
-			select {
-			case <-a:
-				o <- Unit{}
-			case <-b:
-				o <- Unit{}
-			case <-quit:
-				return
-			}
+		for _, input := range inputs {
+			go listenUnit(input.Unit, output.Unit, quit)
 		}
 	case RefTypeTag:
-		a, b := inputA.Ref, inputB.Ref
-		o := output.Ref
-		for {
-			select {
-			case v := <-a:
-				o <- v
-			case v := <-b:
-				o <- v
-			case <-quit:
-				return
-			}
+		for _, input := range inputs {
+			go listenRef(input.Ref, output.Ref, quit)
 		}
 	case SyntaxTypeTag:
-		a, b := inputA.Syntax, inputB.Syntax
-		o := output.Syntax
-		for {
-			select {
-			case v := <-a:
-				o <- v
-			case v := <-b:
-				o <- v
-			case <-quit:
-				return
-			}
+		for _, input := range inputs {
+			go listenSyntax(input.Syntax, output.Syntax, quit)
 		}
 	case ComTypeTag:
-		a, b := inputA.Com, inputB.Com
-		o := output.Com
-		for {
-			select {
-			case v := <-a:
-				o <- v
-			case v := <-b:
-				o <- v
-			case <-quit:
-				return
-			}
+		for _, input := range inputs {
+			go listenCom(input.Com, output.Com, quit)
 		}
 	case StructTypeTag:
-		a, b := inputA.Struct, inputB.Struct
-		o := output.Struct
 		for fieldName, fieldType := range theType.Fields {
-			go runMerge(fieldType, a[fieldName], b[fieldName], o[fieldName], quit)
+			subInputs := make([]Input, len(inputs))
+			for i, input := range inputs {
+				subInputs[i] = input.Struct[fieldName]
+			}
+
+			go runMerge(fieldType, subInputs, output.Struct[fieldName], quit)
 		}
 	}
 }
 
 type MergeCom struct{}
 
-func (MergeCom) OutputType(inputType PartialType) PartialType {
+func (MergeCom) OutputType(inputType PartialType) (ret PartialType) {
 	if inputType.P {
-		return MergePartialTypes(inputType.Fields["a"], inputType.Fields["b"])
-	} else {
-		return PartialType{}
+		for _, subType := range inputType.Fields {
+			ret = MergePartialTypes(ret, subType)
+		}
 	}
+	return
 }
 
 func (MergeCom) Run(inputType DType, input Input, output Output, quit <-chan struct{}) {
-	runMerge(inputType.Fields["a"], input.Struct["a"], input.Struct["b"], output, quit)
+	var elementType DType
+	inputs := make([]Input, len(inputType.Fields))
+	i := 0
+	for fieldName, subType := range inputType.Fields {
+		elementType = subType
+		inputs[i] = input.Struct[fieldName]
+		i++
+	}
+
+	runMerge(elementType, inputs, output, quit)
 }
