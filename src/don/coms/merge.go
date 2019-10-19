295f81e8834099716e97f05de611063c4d09e135
Langdon Holly <langdonzh@gmail.com>
2019-10-19 18:21:09 -0400

    mergeCom runs CompositeCom

--- src/don/coms/merge.go
+++ src/don/coms/merge.go
@@ -2,50 +2,6 @@ package coms
 
 import . "don/core"
 
-func listenUnit(input <-chan Unit, output chan<- Unit, quit <-chan struct{}) {
-	for {
-		select {
-		case <-input:
-			output <- Unit{}
-		case <-quit:
-			return
-		}
-	}
-}
-
-func listenRef(input <-chan Ref, output chan<- Ref, quit <-chan struct{}) {
-	for {
-		select {
-		case val := <-input:
-			output <- val
-		case <-quit:
-			return
-		}
-	}
-}
-
-func runMerge(theType DType, inputs []Input, output Output, quit <-chan struct{}) {
-	switch theType.Tag {
-	case UnitTypeTag:
-		for _, input := range inputs {
-			go listenUnit(input.Unit, output.Unit, quit)
-		}
-	case RefTypeTag:
-		for _, input := range inputs {
-			go listenRef(input.Ref, output.Ref, quit)
-		}
-	case StructTypeTag:
-		for fieldName, fieldType := range theType.Fields {
-			subInputs := make([]Input, len(inputs))
-			for i, input := range inputs {
-				subInputs[i] = input.Struct[fieldName]
-			}
-
-			go runMerge(fieldType, subInputs, output.Struct[fieldName], quit)
-		}
-	}
-}
-
 type MergeCom struct{}
 
 func (MergeCom) OutputType(inputType PartialType) (ret PartialType) {
@@ -58,14 +14,19 @@ func (MergeCom) OutputType(inputType PartialType) (ret PartialType) {
 }
 
 func (MergeCom) Run(inputType DType, input Input, output Output, quit <-chan struct{}) {
-	var elementType DType
-	inputs := make([]Input, len(inputType.Fields))
+	com := CompositeCom{
+		Coms: make([]CompositeComEntry, len(inputType.Fields)),
+		InputMap: SignalReaderIdTree{
+			ParentP:  true,
+			Children: make(map[string]SignalReaderIdTree, len(inputType.Fields))}}
+
 	i := 0
-	for fieldName, subType := range inputType.Fields {
-		elementType = subType
-		inputs[i] = input.Struct[fieldName]
+	for fieldName, _ := range inputType.Fields {
+		com.Coms[i].Com = ICom{}
+		com.InputMap.Children[fieldName] = SignalReaderIdTree{
+			SignalReaderId: SignalReaderId{ReaderId: ReaderId{InternalP: true, InternalIdx: i}}}
 		i++
 	}
 
-	runMerge(elementType, inputs, output, quit)
+	com.Run(inputType, input, output, quit)
 }
