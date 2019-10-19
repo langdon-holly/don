e5e21af839364302263692abf2fa2d3b3389b850
Langdon Holly <langdonzh@gmail.com>
2019-10-19 16:52:44 -0400

    fix SyntaxType

--- src/don/types/syntax.go
+++ src/don/types/syntax.go
@@ -5,12 +5,16 @@ import . "don/core"
 var SyntaxType = MakeStructType(make(map[string]DType, 3))
 
 func init() {
+	SyntaxType.Fields["block?"] = UnitType
 	SyntaxType.Fields["block"] = MakeLinkedListType(*MakeLinkedListType(SyntaxType).Referent)
 
 	mCallFields := make(map[string]DType, 2)
 	mCallFields["macro"] = SyntaxType
 	mCallFields["param"] = SyntaxType
-	SyntaxType.Fields["mcall"] = MakeRefType(MakeStructType(mCallFields))
+	SyntaxType.Fields["macro-call"] = MakeRefType(MakeStructType(mCallFields))
 
-	SyntaxType.Fields["ident"] = BytesType
+	SyntaxType.Fields["ident?"] = UnitType
+	SyntaxType.Fields["select?"] = UnitType
+	SyntaxType.Fields["deselect?"] = UnitType
+	SyntaxType.Fields["field-path"] = *MakeLinkedListType(BytesType).Referent
 }
