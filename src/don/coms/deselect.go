59f3b44a153ea760117d9fdd955adc99a836f7e2
Langdon Holly <langdonzh@gmail.com>
2019-10-19 17:46:04 -0400

    rm type DeselectCom

--- src/don/coms/deselect.go
+++ src/don/coms/deselect.go
@@ -2,18 +2,12 @@ package coms
 
 import . "don/core"
 
-type DeselectCom string
-
-func (gd DeselectCom) OutputType(inputType PartialType) (ret PartialType) {
-	ret.P = true
-	ret.Tag = StructTypeTag
-
-	ret.Fields = make(map[string]PartialType, 1)
-	ret.Fields[string(gd)] = inputType
-
-	return
-}
-
-func (gd DeselectCom) Run(inputType DType, input Input, output Output, quit <-chan struct{}) {
-	RunI(inputType, input, output.Struct[string(gd)], quit)
+func Deselect(fieldName string) Com {
+	return CompositeCom{
+		Coms: []CompositeComEntry{{
+			Com: ICom{},
+			OutputMap: SignalReaderIdTree{
+				SignalReaderId: SignalReaderId{FieldPath: []string{fieldName}}}}},
+		InputMap: SignalReaderIdTree{
+			SignalReaderId: SignalReaderId{ReaderId: ReaderId{InternalP: true}}}}
 }
