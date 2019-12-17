ead86c95cbc57e77a9f81b1b19bfb10c80bacffa
Langdon Holly <langdonzh@gmail.com>
2019-12-17 18:18:22 -0500

    rm some unused code

--- src/don/coms/i.go
+++ src/don/coms/i.go
@@ -14,34 +14,6 @@ func RunI(theType DType, inputGetter InputGetter, outputGetter OutputGetter) {
 		}
 	}
 	return
-
-	//input := inputGetter.GetInput(theType)
-	//output := outputGetter.GetOutput(theType)
-	//
-	//switch theType.Tag {
-	//case UnitTypeTag:
-	//	for {
-	//		select {
-	//		case <-input.Unit:
-	//			output.WriteUnit()
-	//		case <-quit:
-	//			return
-	//		}
-	//	}
-	//case RefTypeTag:
-	//	for {
-	//		select {
-	//		case val := <-input.Ref:
-	//			output.WriteRef(val)
-	//		case <-quit:
-	//			return
-	//		}
-	//	}
-	//case StructTypeTag:
-	//	for fieldName, fieldType := range theType.Fields {
-	//		go RunI(fieldType, input.Struct[fieldName], output.Struct[fieldName], quit)
-	//	}
-	//}
 }
 
 type ICom struct{}
