c7a848a6979cb257a6f3390f0959d5078ce350db
Langdon Holly <langdonzh@gmail.com>
2020-03-17 17:30:29 -0400

    binary And using ProdCom

--- src/don.go
+++ src/don.go
@@ -2,24 +2,38 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 import (
+	"don/coms"
+	. "don/core"
 	"don/extra"
-	"don/syntax"
 	"don/types"
 )
 
 func main() {
-	com := syntax.ParseTop(os.Stdin)[0][0].ToCom()
+	inputTypeFields := make(map[string]DType, 2)
+	inputTypeFields["0"] = types.BoolType
+	inputTypeFields["1"] = types.BoolType
+	inputType := MakeStructType(inputTypeFields)
+	com := coms.And
 
-	input, output, quit := extra.Run(com, types.BoolType)
+	input, output, quit := extra.Run(com, inputType)
 	defer close(quit)
 
-	types.WriteBool(input, true)
+	types.WriteBool(input.Struct["0"], false)
+	types.WriteBool(input.Struct["1"], false)
 	fmt.Println(types.ReadBool(output))
 
-	types.WriteBool(input, false)
+	types.WriteBool(input.Struct["0"], false)
+	types.WriteBool(input.Struct["1"], true)
+	fmt.Println(types.ReadBool(output))
+
+	types.WriteBool(input.Struct["0"], true)
+	types.WriteBool(input.Struct["1"], false)
+	fmt.Println(types.ReadBool(output))
+
+	types.WriteBool(input.Struct["0"], true)
+	types.WriteBool(input.Struct["1"], true)
 	fmt.Println(types.ReadBool(output))
 }
--- src/don/coms/and.go
+++ src/don/coms/and.go
@@ -2,82 +2,15 @@ package coms
 
 import (
 	. "don/core"
-	"don/types"
 )
 
-type AndCom struct{}
-
-func (AndCom) OutputType(inputType DType) DType {
-	switch inputType.Lvl {
-	case UnknownTypeLvl:
-	case NormalTypeLvl:
-		if inputType.Tag != StructTypeTag {
-			return ImpossibleType
-		}
-
-		for _, fieldType := range inputType.Fields {
-			switch fieldType.Lvl {
-			case UnknownTypeLvl:
-			case NormalTypeLvl:
-				if fieldType.Tag != StructTypeTag {
-					return ImpossibleType
-				}
-
-				for _, shouldBeUnit := range []DType{fieldType.Fields["true"], fieldType.Fields["false"]} {
-					switch shouldBeUnit.Lvl {
-					case UnknownTypeLvl:
-					case NormalTypeLvl:
-						if shouldBeUnit.Tag != UnitTypeTag {
-							return ImpossibleType
-						}
-					case ImpossibleTypeLvl:
-						return ImpossibleType
-					}
-				}
-			case ImpossibleTypeLvl:
-				return ImpossibleType
-			}
-		}
-	case ImpossibleTypeLvl:
-		return ImpossibleType
-	}
-
-	return types.BoolType
-}
-
-func (AndCom) Run(inputType DType, inputGetter InputGetter, outputGetter OutputGetter, quit <-chan struct{}) {
-	n := len(inputType.Fields)
-
-	trues := make([]<-chan Unit, n)
-	falses := make([]<-chan Unit, n)
-
-	i := 0
-	for fieldName := range inputType.Fields {
-		trues[i] = inputGetter.Struct[fieldName].Struct["true"].GetInput(UnitType).Unit
-		falses[i] = inputGetter.Struct[fieldName].Struct["false"].GetInput(UnitType).Unit
-		i++
-	}
-
-	oTrue := outputGetter.Struct["true"].GetOutput(UnitType)
-	oFalse := outputGetter.Struct["false"].GetOutput(UnitType)
-
-	for {
-		val := true
-
-		for i := 0; i < n; i++ {
-			select {
-			case <-trues[i]:
-			case <-falses[i]:
-				val = false
-			case <-quit:
-				return
-			}
-		}
-
-		if val {
-			oTrue.WriteUnit()
-		} else {
-			oFalse.WriteUnit()
-		}
-	}
-}
+var And Com = Pipe([]Com{
+	ProdCom{},
+	SplitMerge([]Com{
+		Pipe([]Com{SelectCom("true"), SelectCom("true"), Deselect("true")}),
+		Pipe([]Com{
+			SplitMerge([]Com{
+				Pipe([]Com{SelectCom("true"), SelectCom("false")}),
+				Pipe([]Com{SelectCom("false"), SelectCom("true")}),
+				Pipe([]Com{SelectCom("false"), SelectCom("false")})}),
+			Deselect("false")})})})
