07adb4a421660186fe32a113c3fff66b56af1006
Langdon Holly <langdonzh@gmail.com>
2020-12-12 18:07:37 -0500

    Syntax String()

--- src/don/syntax/printer.go
+++ src/don/syntax/printer.go
@@ -2,30 +2,64 @@ package syntax
 
 import "strings"
 
-func (s Syntax) String() string {
+func subSyntaxString(out *strings.Builder, s Syntax, indent []byte, precedence SyntaxTag) {
+	if s.Tag < precedence {
+		out.WriteString("(")
+		syntaxString(out, s, indent, false)
+		out.WriteString(")")
+	} else if syntaxString(out, s, indent, false); true {
+	}
+}
+func syntaxString(out *strings.Builder, s Syntax, indent []byte, topLevel bool) {
 	switch s.Tag {
+	case ListSyntaxTag:
+		subIndent := indent
+		if !topLevel {
+			subIndent = append(indent, byte('\t'))
+		}
+		out.WriteString("\n")
+		for _, line := range s.Children {
+			out.Write(subIndent)
+			subSyntaxString(out, line, subIndent, ListSyntaxTag+1)
+			out.WriteString("\n")
+		}
+	case SpacedSyntaxTag:
+		subSyntaxString(out, s.Children[0], indent, SpacedSyntaxTag+1)
+		for i := 1; i < len(s.Children); i++ {
+			out.WriteString(" ")
+			subSyntaxString(out, s.Children[i], indent, SpacedSyntaxTag+1)
+		}
+	case MCallSyntaxTag:
+		subSyntaxString(out, s.Children[0], indent, SandwichSyntaxTag+1)
+		out.WriteString("!")
+		subSyntaxString(out, s.Children[1], indent, MCallSyntaxTag)
+	case SandwichSyntaxTag:
+		subSyntaxString(out, s.Children[0], indent, SandwichSyntaxTag+1)
+		out.WriteString("-")
+		subSyntaxString(out, s.Children[1], indent, MCallSyntaxTag)
 	case NameSyntaxTag:
-		var builder strings.Builder
 		if s.LeftMarker {
-			builder.WriteString(":")
+			out.WriteString(":")
 		}
 		if s.Name != "" {
 			for _, b := range []byte(s.Name) {
 				if byteIsSpecial(b) {
-					builder.WriteString("\\")
+					out.WriteString("\\")
 				}
-				builder.WriteByte(b)
+				out.WriteByte(b)
 			}
-		} else if builder.WriteString("_"); true {
+		} else if out.WriteString("_"); true {
 		}
 		if s.RightMarker {
-			builder.WriteString(":")
+			out.WriteString(":")
 		}
-		return builder.String()
-	default:
-		panic("Unimplemented")
 	}
-	panic("Unreachable")
+	return
+}
+func (s Syntax) String() string {
+	var b strings.Builder
+	syntaxString(&b, s, nil, true)
+	return b.String()
 }
 
 func EscapeFieldName(fieldName string) string {
