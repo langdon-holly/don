598f448219556f22a3adfb87c39e429bb35fd05e
Langdon Holly <langdonzh@gmail.com>
2023-03-12 03:19:24 +0000

    Preserve impossible type info

--- src/rel/type.go
+++ src/rel/type.go
@@ -27,10 +27,6 @@ func typePtrTo(t Type) *TypePtr {
 	tp := TypePtr(typePtrRoot{t: t})
 	return &tp
 }
-func DebugTypePtr() *TypePtr {
-	tp := TypePtr(typePtrRoot{t: AnyType{}})
-	return &tp
-}
 func UnifyTypePtrs(r, s *TypePtr /* mutated */) {
 	rTP, rRoot := typePtrGetRoot(r)
 	sTP, sRoot := typePtrGetRoot(s)
@@ -57,30 +53,32 @@ func CopyTypePtr(r *TypePtr, mapping map[*TypePtr]*TypePtr /* mutated */) *TypeP
 
 // Type
 
-type Type interface {
-	get(Junctive, string) *TypePtr
-	copy(map[*TypePtr]*TypePtr) Type
-	Syntax() Syntax /* Mutates */
-	String() string /* Mutates */
+type Type struct {
+	Unit      bool
+	Conjuncts map[string]*TypePtr
+	Disjuncts map[string]*TypePtr
 }
-
-type AnyType struct{}
-type JunctiveType struct {
-	Junctive
-	Juncts map[string]*TypePtr /* Non-nil */
-}
-type NoType struct{}
 
 // Get Type
 
-var UnitType Type = AnyType{}
+func AnyTypePtr() *TypePtr {
+	return typePtrTo(
+		Type{Conjuncts: make(map[string]*TypePtr, 0), Disjuncts: make(map[string]*TypePtr, 0)},
+	)
+}
 
-func AnyTypePtr() *TypePtr { return typePtrTo(AnyType{}) }
-func NoTypePtr() *TypePtr  { return typePtrTo(NoType{}) }
 func TypePtrAt(junctive Junctive, fieldName string, tp *TypePtr) *TypePtr {
-	u := JunctiveType{Junctive: junctive, Juncts: make(map[string]*TypePtr, 1)}
-	u.Juncts[fieldName] = tp
-	return typePtrTo(u)
+	juncts := make(map[string]*TypePtr, 1)
+	juncts[fieldName] = tp
+	var conjuncts, disjuncts map[string]*TypePtr
+	if junctive == ConJunctive {
+		conjuncts = juncts
+		disjuncts = make(map[string]*TypePtr, 0)
+	} else /* junctive == DisJunctive */ {
+		conjuncts = make(map[string]*TypePtr, 0)
+		disjuncts = juncts
+	}
+	return typePtrTo(Type{Conjuncts: conjuncts, Disjuncts: disjuncts})
 }
 func AtLeft(t *TypePtr) *TypePtr  { return TypePtrAt(ConJunctive, "0", t) }
 func AtRight(t *TypePtr) *TypePtr { return TypePtrAt(ConJunctive, "1", t) }
@@ -97,21 +95,18 @@ func PairTypePtr() *TypePtr {
 	return t
 }
 
-// Mutates
-func (AnyType) get(junctive Junctive, fieldName string) *TypePtr {
-	panic("Get field of any")
-}
-func (jt JunctiveType) get(junctive Junctive, fieldName string) *TypePtr {
-	if jt.Junctive != junctive {
-		panic("Wrong junctive")
-	} else if fieldTypePtr, ok := jt.Juncts[fieldName]; ok {
+func (t Type) get(junctive Junctive, fieldName string) *TypePtr {
+	var juncts map[string]*TypePtr
+	if junctive == ConJunctive {
+		juncts = t.Conjuncts
+	} else /* junctive == DisJunctive */ {
+		juncts = t.Disjuncts
+	}
+	if fieldTypePtr, ok := juncts[fieldName]; ok {
 		return fieldTypePtr
 	} else {
 		panic("Nonexistent field")
 	}
-}
-func (NoType) get(junctive Junctive, fieldName string) *TypePtr {
-	return NoTypePtr()
 }
 
 // May not round-trip
@@ -124,55 +119,80 @@ func ConvertTypePtr(t *TypePtr) *TypePtr {
 // Other
 
 func joinTypes(t0, t1 Type /* consumed in places where both are junctive */) Type {
-	if _, t0Any := t0.(AnyType); false {
-	} else if _, t1No := t1.(NoType); t0Any || t1No {
-		return t1
-	} else if _, t1Any := t1.(AnyType); false {
-	} else if _, t0No := t0.(NoType); t1Any || t0No {
-		return t0
-	} else if j0 := t0.(JunctiveType); false {
-	} else if j1 := t1.(JunctiveType); j0.Junctive != j1.Junctive {
-	} else {
-		for fieldName, fieldTypePtr1 := range j1.Juncts {
-			if fieldTypePtr0, ok := j0.Juncts[fieldName]; ok {
-				UnifyTypePtrs(fieldTypePtr0, fieldTypePtr1)
-			}
-			j0.Juncts[fieldName] = fieldTypePtr1
+	t0.Unit = t0.Unit || t1.Unit
+	for fieldName, fieldTypePtr1 := range t1.Conjuncts {
+		if fieldTypePtr0, ok := t0.Conjuncts[fieldName]; ok {
+			UnifyTypePtrs(fieldTypePtr0, fieldTypePtr1)
 		}
-		return j0
+		t0.Conjuncts[fieldName] = fieldTypePtr1
 	}
-	return NoType{}
+	for fieldName, fieldTypePtr1 := range t1.Disjuncts {
+		if fieldTypePtr0, ok := t0.Disjuncts[fieldName]; ok {
+			UnifyTypePtrs(fieldTypePtr0, fieldTypePtr1)
+		}
+		t0.Disjuncts[fieldName] = fieldTypePtr1
+	}
+	return t0
 }
 
-func (AnyType) copy(_ map[*TypePtr]*TypePtr) Type { return AnyType{} }
-func (j JunctiveType) copy(mapping map[*TypePtr]*TypePtr) Type {
-	juncts := make(map[string]*TypePtr, len(j.Juncts))
-	for fieldName, fieldTypePtr := range j.Juncts {
-		juncts[fieldName] = CopyTypePtr(fieldTypePtr, mapping)
+func (t Type) copy(mapping map[*TypePtr]*TypePtr) Type {
+	conjuncts := make(map[string]*TypePtr, len(t.Conjuncts))
+	for fieldName, fieldTypePtr := range t.Conjuncts {
+		conjuncts[fieldName] = CopyTypePtr(fieldTypePtr, mapping)
 	}
-	return JunctiveType{Junctive: j.Junctive, Juncts: juncts}
+	disjuncts := make(map[string]*TypePtr, len(t.Disjuncts))
+	for fieldName, fieldTypePtr := range t.Disjuncts {
+		disjuncts[fieldName] = CopyTypePtr(fieldTypePtr, mapping)
+	}
+	return Type{Unit: t.Unit, Conjuncts: conjuncts, Disjuncts: disjuncts}
 }
-func (NoType) copy(_ map[*TypePtr]*TypePtr) Type { return NoType{} }
 
 // Mutates
-func (_ AnyType) Syntax() Syntax { return NameSyntax("I") }
-func (j JunctiveType) Syntax() Syntax {
-	var junctWordses [][]syntax.Word
-	for fieldName, fieldTypePtr := range j.Juncts {
-		junctWordses = append(
-			junctWordses,
-			[]syntax.Word{
-				CollectSyntax(fieldName, j.Junctive),
-				TypePtrType(fieldTypePtr).Syntax().Word(),
-				SelectSyntax(fieldName, j.Junctive),
-			},
-		)
+func (t Type) Syntax() Syntax {
+	var typeConjuncts []Syntax
+	if t.Unit {
+		typeConjuncts = append(typeConjuncts, NameSyntax("unit"))
 	}
-	return JunctionSyntax(j.Junctive, junctWordses)
+	if len(t.Conjuncts) > 0 {
+		var conjunctWordses [][]syntax.Word
+		for fieldName, fieldTypePtr := range t.Conjuncts {
+			conjunctWordses = append(
+				conjunctWordses,
+				[]syntax.Word{
+					CollectSyntax(fieldName, ConJunctive),
+					TypePtrType(fieldTypePtr).Syntax().Word(),
+					SelectSyntax(fieldName, ConJunctive),
+				},
+			)
+		}
+		typeConjuncts = append(typeConjuncts, JunctionSyntax(ConJunctive, conjunctWordses))
+	}
+	if len(t.Disjuncts) > 0 {
+		var disjunctWordses [][]syntax.Word
+		for fieldName, fieldTypePtr := range t.Disjuncts {
+			disjunctWordses = append(
+				disjunctWordses,
+				[]syntax.Word{
+					CollectSyntax(fieldName, DisJunctive),
+					TypePtrType(fieldTypePtr).Syntax().Word(),
+					SelectSyntax(fieldName, DisJunctive),
+				},
+			)
+		}
+		typeConjuncts = append(typeConjuncts, JunctionSyntax(DisJunctive, disjunctWordses))
+	}
+	if len(typeConjuncts) == 0 {
+		return NameSyntax("I")
+	} else if len(typeConjuncts) == 1 {
+		return typeConjuncts[0]
+	} else {
+		typeConjunctWordses := make([][]syntax.Word, len(typeConjuncts))
+		for i, typeConjunct := range typeConjuncts {
+			typeConjunctWordses[i] = typeConjunct.Composition()
+		}
+		return JunctionSyntax(ConJunctive, typeConjunctWordses)
+	}
 }
-func (_ NoType) Syntax() Syntax { return NameSyntax("false") }
 
 // Mutates
-func (t AnyType) String() string      { return t.Syntax().String() }
-func (t JunctiveType) String() string { return t.Syntax().String() }
-func (t NoType) String() string       { return t.Syntax().String() }
+func (t Type) String() string { return t.Syntax().String() }
